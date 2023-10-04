25243c97d9ca3e119f679233ccda219aa85dc901
mka <mka@A22PC5>
2023-10-04 20:54:00 +0300

    Add project files.

---
+++ WpfApp2/Window1.xaml.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace WpfApp2
+{
+    /// <summary>
+    /// Interaction logic for Window1.xaml
+    /// </summary>
+    public partial class Window1 : Page
+    {
+        public string myMailAddress { get; } = "tmvlad33@gmail.com";
+        public string accountPassword { get; } = "gxknljmktrlthlyx";
+        
+        MailMessage mail = new();
+        
+        public Window1()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            mail = new MailMessage(myMailAddress, toTxtBox.Text)
+            {
+                Subject = subjectTxtBox.Text,
+                Body = $"<h1>My Mail Message from C#</h1><p>{bodyTxtBox.Text}</p>",
+                IsBodyHtml = true,
+                Priority = MailPriority.Normal
+            };
+
+            SmtpClient client = new SmtpClient("smtp.gmail.com", 587)
+            {
+                Credentials = new NetworkCredential(myMailAddress, accountPassword),
+                EnableSsl = true
+            };
+
+            client.Send(mail);
+
+        }
+
+        private void AddAttachments_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog dialog = new OpenFileDialog();
+            if (dialog.ShowDialog() == true)
+                mail.Attachments.Add(new Attachment(dialog.FileName));
+        }
+
+        private void isImportant_Checked(object sender, RoutedEventArgs e)
+        {
+            if (isImportant.IsChecked == true)
+            {
+                mail.Priority = MailPriority.High;
+            }
+            else mail.Priority = MailPriority.Normal;
+        }
+    }
+}
